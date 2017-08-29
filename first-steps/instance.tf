e6b301d17cf65f1ad2cc518f954bd1a69084bc8e
Ryan Gooler <ryan.gooler@gmail.com>
2017-08-29 01:32:31 -0700

    First lab

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,10 +1,8 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  region = "us-east-2"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-dbbd9dbe"
   instance_type = "t2.micro"
 }
