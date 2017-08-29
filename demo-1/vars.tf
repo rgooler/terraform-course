c791147160172419195a0f0102db2c5306ffec01
Ryan Gooler <ryan.gooler@gmail.com>
2017-08-29 01:42:38 -0700

    Update notes

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,13 +1,14 @@
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
+
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1 = "ami-1d4e7a66"
+    us-east-2 = "ami-dbbd9dbe"
+    us-west-1 = "ami-969ab1f6"
+    us-west-2 = "ami-8803e0f0"
+    eu-west-1 = "ami-674cbc1e"
   }
 }
